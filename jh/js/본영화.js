49221cd88f16d9b4502e5fe07ab15d7d398ffd29
LJAEH <wogurdlek33@gmail.com>
2023-04-04 15:09:13 +0900

    0404

--- jh/js/본영화.js
+++ jh/js/본영화.js
@@ -1,6 +1,23 @@
-const popUp = document.getElementById("popUp");
+const watchedMenu = document.getElementById("watchedMenu");
+const modalOverlay = document.getElementById("modalOverlay");
+const nav = document.getElementsByClassName("nav");
+const modal = document.getElementsByClassName("modal");
 
-document.getElementById("menu")
-.addEventListener("click", function(){
-    popUp.style.display='inline';
+
+watchedMenu.addEventListener("click", function(){
+    modalOverlay.style.display = "flex";
+    nav[0].style.display = "none";
+
 })
+
+
+modalOverlay.addEventListener("click", e => {
+    const evTarget = e.target
+    if(evTarget.classList.contains("modal")) {
+        modalOverlay.style.display = "none"
+       
+    }
+    nav[0].style.display = "flex";
+})
+
+

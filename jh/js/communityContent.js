7356bc76f5e8164276f47651d5d7dac4d4793238
JungWook <kjo6391@gmail.com>
2023-04-12 10:17:10 +0900

    reset

--- jh/js/communityContent.js
+++
@@ -1,9 +0,0 @@
-// const communityBar = document.getElementById("communityBar");
-
-// window.addEventListener("resize", function(){
-//     if(window.innerWidth <= 700){
-//         communityBar.style.display = "none";
-//     } else {
-//         communityBar.style.display = "flex";
-//     }
-// })
--- jw/JAVA/community/src/main/java/ms/community/board/model/vo/BoardImage.java
+++
@@ -1,8 +0,0 @@
-package ms.community.board.model.vo;
-
-public class BoardImage {
-	private int imgNo;
-	private String imgRename;
-	private String fileName;
-	private int comNo;
-}
